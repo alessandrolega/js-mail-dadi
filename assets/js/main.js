6676f0e71d4d5bb9d7421148774ff321e497d085
alessandrolega <alessandrolega24@gmail.com>
2022-10-24 18:02:48 +0200

    aggiunto array e funzione sul button

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+// Mail
+
+// Chiedi all’utente la sua email,
+// controlla che sia già presente nella lista di email per controllare se può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+let MailList = ["js@boolean.com", "css@boolean.com", "html@boolean.com"];
+console.log(MailList)
+
+function verificaEmail () {
+    let Mail = document.getElementById('inputEmail').value;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// scrivere quante partite ha vinto il giocatore
+// scrivere quante partite ha vinto il computer
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
