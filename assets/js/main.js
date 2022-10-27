bf40fbb3587455b90ec9e7e967283386d7743b65
alessandrolega <alessandrolega24@gmail.com>
2022-10-27 03:51:57 +0200

    aggiunto esercizio dadi (da completare)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,54 @@
-// Mail
+// Mail CONSEGNA
 
 // Chiedi all’utente la sua email,
 // controlla che sia già presente nella lista di email per controllare se può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-let inputEmail = document.getElementById('inputEmail');
-let emails = ['html@boolean.it', 'css@boolean.it', 'js@boolean.it'];
 
-let emailPresent = false;
+// DADI CONSEGNA
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// scrivere quante partite ha vinto il giocatore
+// scrivere quante partite ha vinto il computer
+
+// MAIL SVOLGIMENTO
+
+// let inputEmail = document.getElementById('inputEmail');
+// let emails = ['html@boolean.it', 'css@boolean.it', 'js@boolean.it'];
+
+// let emailPresent = true;
 
 
-function verificaEmail(){
+// function verificaEmail(){
 
-    let valueEmail = inputEmail.value;
+//     let valueEmail = inputEmail.value;
     
-    for (let i=0; i < emails.length; i++) {
+//     for (let i=0; i < emails.length; i++) {
+//     if (valueEmail = emails[i]) {
+//         document.getElementById('text').innerText = 'success';
 
-        let thisEmail = emails[i];
-        
-    if (valueEmail = thisEmail) {
+//         emailPresent = true;
 
-        emailPresent = true;
+//     } else{
+//         document.getElementById('text').innerText = 'error';
 
-    } else{
-        document.getElementById('text').innerText = 'error';
-    }
+//         emailPresent = false;
+//     }
 
-    }
+//     }
 
-}
+// }
+
+
+
+// DADI SVOLGIMENTO
+
+let player = Math.round(Math.random() *6) +1;
+let computer = Math.round(Math.random() *6) +1;
+
+
 
 
 
